6e5c5c2e0716f91e817d7929feadcaee3376dad2
Nick Warren <nickwarren47@gmail.com>
2022-07-04 11:14:36 -0600

    Done with assignment

--- index.js
+++ index.js
@@ -1,29 +1,61 @@
 
+// const employee = {
+//     name: "Bob",
+//     address: "22 Chucklehead Ln.",
+// }
+
+
+// function  updateEmployeeWithKeyAndValue(employee, key, value) {
+// return {
+//     ...employee,
+// [key]: value,
+// }
+// }
+
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+//     employee[key] = value;
+//     return employee
+// }
+
+// function deleteFromEmployeeByKey(employee, key, value) {
+//     const newEmployee = {...employee}
+//     newEmployee[key] = value;
+//     return newEmployee
+// }
+
+// function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+//     employee[key] = value;
+//     return employee;
+// }
+
+
 const employee = {
-    name: "Bob",
-    address: "22 Chucklehead Ln.",
+    name: 'bob',
+    streetAddress: "45 Chucklehead Way"
 }
 
-
-function  updateEmployeeWithKeyAndValue(employee, key, value) {
-return {
+function updateEmployeeWithKeyAndValue(employee, key, value){
+   return{
     ...employee,
-[key]: value,
-}
-}
-
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee[key] = value;
-    return employee
+    [key]: value,
+   }
 }
 
-function deleteFromEmployeeByKey(employee, key, value) {
-    const newEmployee = {...employee}
-    newEmployee[key] = value;
-    return newEmployee
-}
-
-function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
     employee[key] = value;
     return employee;
+};
+
+function deleteFromEmployeeByKey(employee, key, value){
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
 }
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value){
+   employee[key] = value;
+   return employee;
+}
+
+
+
